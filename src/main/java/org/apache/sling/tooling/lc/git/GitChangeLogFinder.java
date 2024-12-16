ae0089c4de468a9eeddbf6124821bf06e6905999
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-12-16 13:33:43 +0100

    apply spotless formatting

--- src/main/java/org/apache/sling/tooling/lc/git/GitChangeLogFinder.java
+++ src/main/java/org/apache/sling/tooling/lc/git/GitChangeLogFinder.java
@@ -1,18 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with this
- * work for additional information regarding copyright ownership. The ASF
- * licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.sling.tooling.lc.git;
 
@@ -30,14 +32,15 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
 public class GitChangeLogFinder {
-    
-    public static void main(String[] args) throws IOException, GitAPIException  {
-        new GitChangeLogFinder("..").getChanges("org.apache.sling.adapter", "2.1.2", "2.1.6")
-            .stream().forEach(System.out::println);
+
+    public static void main(String[] args) throws IOException, GitAPIException {
+        new GitChangeLogFinder("..")
+                .getChanges("org.apache.sling.adapter", "2.1.2", "2.1.6").stream()
+                        .forEach(System.out::println);
     }
-    
+
     private final String slingRepoCheckoutDir;
-    
+
     /**
      * @param slingRepoCheckoutDir the <tt>repo</tt> root for Apache Sling
      */
@@ -46,12 +49,13 @@ public class GitChangeLogFinder {
     }
 
     public List<String> getChanges(String artifactId, String from, String to) throws IOException, GitAPIException {
-        
+
         Path repoPath = Paths.get(slingRepoCheckoutDir, artifactId.replace('.', '-'), ".git");
-        
+
         FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
-        
-        Repository repository = repositoryBuilder.setGitDir(repoPath.toFile())
+
+        Repository repository = repositoryBuilder
+                .setGitDir(repoPath.toFile())
                 .readEnvironment() // scan environment GIT_* variables
                 .findGitDir() // scan up the file system tree
                 .setMustExist(true)
@@ -59,26 +63,26 @@ public class GitChangeLogFinder {
 
         Ref fromTag = getTagChecked(repository, artifactId, from);
         Ref toTag = getTagChecked(repository, artifactId, to);
-        
+
         Git git = Git.wrap(repository);
-        
+
         fromTag = repository.getRefDatabase().peel(fromTag);
         toTag = repository.getRefDatabase().peel(toTag);
-        
+
         List<String> commits = new ArrayList<>();
         git.log()
-            .addRange(fromTag.getPeeledObjectId(), toTag.getPeeledObjectId())
-            .call()
-            .forEach( c -> commits.add(c.getShortMessage()));
-        
+                .addRange(fromTag.getPeeledObjectId(), toTag.getPeeledObjectId())
+                .call()
+                .forEach(c -> commits.add(c.getShortMessage()));
+
         return commits;
     }
 
     private Ref getTagChecked(Repository repository, String artifactId, String version) throws IOException {
-        
+
         final String tagName = artifactId + "-" + version;
         final Ref ref = repository.getRefDatabase().getRef(Constants.R_TAGS + tagName);
-        if ( ref == null )
+        if (ref == null)
             throw new RuntimeException("No tag " + tagName + " found in git repo at " + repository.getDirectory());
         return ref;
     }
--- src/main/java/org/apache/sling/tooling/lc/jira/Issue.java
+++ src/main/java/org/apache/sling/tooling/lc/jira/Issue.java
@@ -1,18 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with this
- * work for additional information regarding copyright ownership. The ASF
- * licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.sling.tooling.lc.jira;
 
@@ -20,10 +22,9 @@ import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class Issue implements Comparable<Issue> {
-    
-    private static final Pattern KEY_PATTERN = Pattern.compile("^([A-Z]+)-(\\d+)$"); 
+
+    private static final Pattern KEY_PATTERN = Pattern.compile("^([A-Z]+)-(\\d+)$");
 
     private final String key;
     private final Fields fields;
@@ -36,15 +37,15 @@ public class Issue implements Comparable<Issue> {
     public String getKey() {
         return key;
     }
-    
+
     public Fields getFields() {
         return fields;
     }
-    
+
     public String getSummary() {
         return fields.getSummary();
     }
-    
+
     @Override
     public String toString() {
         return key + " - " + getSummary();
@@ -52,26 +53,25 @@ public class Issue implements Comparable<Issue> {
 
     @Override
     public int compareTo(Issue o) {
-        
+
         Matcher ourMatcher = KEY_PATTERN.matcher(key);
         Matcher theirMatcher = KEY_PATTERN.matcher(o.key);
-        
-        if ( !ourMatcher.matches()) {
+
+        if (!ourMatcher.matches()) {
             throw new IllegalArgumentException("No match found for " + key);
         }
 
-        if ( !theirMatcher.matches()) {
+        if (!theirMatcher.matches()) {
             throw new IllegalArgumentException("No match found for " + o.key);
         }
-        
+
         String ourProject = ourMatcher.group(1);
         String theirProject = theirMatcher.group(1);
-        
-        
-        if ( !Objects.equals(ourProject, theirProject)) {
+
+        if (!Objects.equals(ourProject, theirProject)) {
             return ourProject.compareTo(theirProject);
         }
-        
+
         int ourId = Integer.parseInt(ourMatcher.group(2));
         int theirId = Integer.parseInt(theirMatcher.group(2));
 
